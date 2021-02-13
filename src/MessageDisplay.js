54ce69a85bddd0a0b835beb88287a35513ff8f91
Sera_Is <isonserena@hotmail.it>
2021-02-13 23:01:26 +0100

    UI adjustments

--- src/MessageDisplay.js
+++ src/MessageDisplay.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./MessageDisplay.css";
 
 export default function MessageDisplay() {
+    
     return (
         <div className="MessageDisplay">
             <p>
