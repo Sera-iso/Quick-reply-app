c16506175aaf6ce89bfedf11cf51c8a757566f5c
Sera <74925057+Sera-iso@users.noreply.github.com>
2021-06-03 22:20:52 +0200

    Added "Work in progress message"

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1>
-        Quick reply app
+        Quick reply app 
+        <br /> 
+        Work in progress... I will continue development as soon as I learn more about database requests / responses.
       </h1>
       <MessageInput />
     </div>
--- src/MessageInput.js
+++ src/MessageInput.js
@@ -1,6 +1,6 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import MessageDisplay from "./MessageDisplay";
-//import { PostgrestClient } from '@supabase/postgrest-js';
+//import { createClient } from '@supabase/supabase-js'
 import "./MessageInput.css";
 
 
@@ -11,21 +11,27 @@ export default function MessageInput() {
     const [text, setText] = useState("");
     function handleSubmit(event) {
         event.prevent.default();
+        /*const supabase = createClient('https://wrcjrjorncqjgbiytslr.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxMzI4OTEwMSwiZXhwIjoxOTI4ODY1MTAxfQ.gKbsvpOSRKRit97s722urmfiOtbuZtA8rabHE8_NWNk');
+        supabase
+            .from('stories')
+            .insert([
+                { parts: JSON.stringify([]) }
+            ]).then(function()) */
     }
     function displayText(event) {
         setText(event.target.value);
     }
     return (
         <div>
-        <div className="MessageInput">
-            <form onSubmit={handleSubmit}>
-            <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Write something" onChange={displayText} />
-                <button type="submit" className="btn btn-primary">Send</button>
+            <div className="MessageInput">
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group mb-3">
+                        <input type="text" className="form-control" placeholder="Write something" onChange={displayText} />
+                        <button type="submit" className="btn btn-primary">Send</button>
+                    </div>
+                </form>
             </div>
-            </form>
-        </div>
-        <MessageDisplay content={text} />
+            <MessageDisplay content={text} />
         </div>
     )
 }
